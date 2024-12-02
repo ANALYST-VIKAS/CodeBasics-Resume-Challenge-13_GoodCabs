4db12f5e95ccc2daf8b55c050fb4542d34cac2e4
Vikas Yadav <152646961+ANALYST-VIKAS@users.noreply.github.com>
2024-12-02 10:39:07 +0530

    Update primary and secondary analysis.sql

--- primary and secondary analysis.sql
+++ primary and secondary analysis.sql
@@ -11,10 +11,7 @@ GROUP BY city_id
 dim_city c ON c.city_id=t.city_id
 WHERE h_rnk <=3 OR h_rnk>=8
 ORDER BY ROUND(total_trips/total*100,2) DESC;
--- ------------------------------------------------------------------------------------------------------------------------------------------------
--- CALCULATE THE AVERAGE FARE PER TRIP FOR EACH CITY AND COMPARE IT WITH THE CITY'S AVERAGE
--- TRIP DISTANCE.IDENTIFY THE CITIES WITH THE HIGHEST AND LOWEST AVERAGE FARE PER TRIP TO 
--- ASSESS PRICING EFFICIENCY ACROSS LOCATIONS.
+-- CALCULATE THE AVERAGE FARE PER TRIP FOR EACH CITY AND COMPARE IT WITH THE CITY'S AVERAGE TRIP DISTANCE.IDENTIFY THE CITIES WITH THE HIGHEST AND LOWEST AVERAGE FARE PER TRIP TO  ASSESS PRICING EFFICIENCY ACROSS LOCATIONS.
 WITH CTE AS
 (
 SELECT c.city_id,c.city_name,
@@ -27,9 +24,8 @@ ORDER BY 2 ASC
 )
 SELECT city_name,average_distance,average_fare FROM CTE
 ORDER BY  average_fare ASC;
--- -------------------------------------------------------------------------------------------------------------------------------------------------
--- CALCULATE THE AVERAGE PASSENGER AND DRIVER RATING FOR EACH CITY,SEGMENTED,BY PASSENGER TYPE (NEW VS REPEAT)
--- IDENTIFY THE CITIES WITH HIGHEST AND LOWEST AVERAGE RATINGS.
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- CALCULATE THE AVERAGE PASSENGER AND DRIVER RATING FOR EACH CITY,SEGMENTED,BY PASSENGER TYPE (NEW VS REPEAT)IDENTIFY THE CITIES WITH HIGHEST AND LOWEST AVERAGE RATINGS.
 -- Passenger Rating --
 SELECT city_name AS City,passenger_type,ROUND(avg_passenger_rating,2) AS average_passenger_rating FROM
 (
@@ -57,10 +53,8 @@ GROUP BY c.city_name,passenger_type
 order by 4 desc
 ) t 
 WHERE d_rnk=1 OR d_rnk=19;
--- PEAK AND LOW DEMAND MONTHS BY CITY
--- FOR EACH CITY IDENTIFY THE MONTH WITH THE HIGHEST TOTAL TRIPS(PEAK DEMAND) AND THE MONTH WITH LOWEST TRIPS
--- (LOW DEMAND).THIS ANALYSIS WILL HELP GOOD CABS TO UNDERSTAND SEASONAL PATTERNS AND ADJUST RESOURCES
--- ACCORDINGLY.
+-- PEAK AND LOW DEMAND MONTHS BY CITY FOR EACH CITY IDENTIFY THE MONTH WITH THE HIGHEST TOTAL TRIPS(PEAK DEMAND) AND THE MONTH WITH LOWEST TRIPS(LOW DEMAND).
+-- THIS ANALYSIS WILL HELP GOOD CABS TO UNDERSTAND SEASONAL PATTERNS AND ADJUST RESOURCES ACCORDINGLY.
 WITH CTE1 AS
 (
 WITH CTE AS
@@ -105,9 +99,7 @@ ORDER BY d.city_name,trips DESC
 SELECT city_name AS City,day_type AS Day_Type,CONCAT(ROUND(trips*100/total_trips,2),"%") AS percent_of_trips
 FROM CTE1;
 -- ------------------------------------------------------------------------------------------------------------------------------------------------
--- REPEAT PASSENGER FREQUENCY AND CITY CONTRIBUTIONS ANALYSIS
--- ANALYSE THE FREQUECY OF TRIPS TAKEN BY REPEAT PASSENGERS IN EACH CITY.
--- PERCENT OF REPEAT PASSENGERS TAKING 2 TRIPS,3 TRIPS ETC..........
+-- REPEAT PASSENGER FREQUENCY AND CITY CONTRIBUTIONS ANALYSIS ANALYSE THE FREQUECY OF TRIPS TAKEN BY REPEAT PASSENGERS IN EACH CITY.PERCENT OF REPEAT PASSENGERS TAKING 2 TRIPS,3 TRIPS ETC..........
 WITH CTE AS
 (
 SELECT *,
@@ -127,11 +119,8 @@ dim_city d ON c.city_id=d.city_id
 WHERE d_rnk<=2
 GROUP BY city_name;
 -- ------------------------------------------------------------------------------------------------------------------------------------------------
--- MONTHLY TARGET ACHIEVEMENTS ANALSIS OF KEY METRICS
--- FOR EACH CITY,EVALUATE MONTHLY PERFORMANCE AGAINST TARGETS FOR TOTAL TRIPS NEW PASSENGERS,AND AVERAGE PASSENGER
--- CALCULATE THE  PERCENTAGE DIFFERENCE ANY CONSISTENT PATTERNS IN TARGET ACHEIVEMENT PARTICULARLY ACROSS TOURISM
--- VERSUS BUSSINESS FOCUSED COUNTRIES.
--- NEW PASSENGER TARGETS-- 
+-- MONTHLY TARGET ACHIEVEMENTS ANALSIS OF KEY METRICS FOR EACH CITY,EVALUATE MONTHLY PERFORMANCE AGAINST TARGETS FOR TOTAL TRIPS NEW PASSENGERS,AND AVERAGE PASSENGER
+-- CALCULATE THE  PERCENTAGE DIFFERENCE ANY CONSISTENT PATTERNS IN TARGET ACHEIVEMENT PARTICULARLY ACROSS TOURISM VERSUS BUSSINESS FOCUSED COUNTRIES.NEW PASSENGER TARGETS
 -- TOURISM BASED CITIES --
 SELECT c.city_name,MONTHNAME(fps.month) AS month,fps.new_passengers AS np,tnp.target_new_passengers AS tnp,
 ROUND((1-tnp.target_new_passengers/fps.new_passengers)*100,2) AS pct_difference
@@ -142,8 +131,8 @@ INNER JOIN dim_city c ON c.city_id=fps.city_id
 WHERE c.city_name IN ("Jaipur","Chandigarh","Mysore","Kochi")
 ORDER BY city_name,month(fps.month);
 
+-- BUSSINESS BASED CITIES------
 
--- BUSSINESS BASED CITIES
 SELECT c.city_name,MONTHNAME(fps.month) AS month,fps.new_passengers AS np,tnp.target_new_passengers AS tnp,
 ROUND((1-tnp.target_new_passengers/fps.new_passengers)*100,2) AS pct_difference
 FROM fact_passenger_summary fps 
@@ -152,7 +141,10 @@ targets_db.monthly_target_new_passengers tnp ON fps.month=tnp.month AND fps.city
 INNER JOIN dim_city c ON c.city_id=fps.city_id
 WHERE c.city_name NOT IN ("Jaipur","Chandigarh","Mysore","Kochi")
 ORDER BY city_name,month(fps.month);
+
 -- NEW TRIP TARGETS--
+
+
 -- TOURISM BASED CITIES-- 
 SELECT c.city_name, MONTHNAME(tp.month) AS month,tp.total_trips,tgt.total_target_trips,
 ROUND((1-tgt.total_target_trips/tp.total_trips)*100,2) AS  pct_difference
@@ -180,7 +172,10 @@ targets_db.monthly_target_trips tgt ON tgt.month=tp.month AND tgt.city_id=tp.cit
 INNER JOIN dim_city c ON c.city_id=tp.city_id
 WHERE c.city_name NOT IN ("Jaipur","Kochi","Mysore","Chandigarh")
 ORDER BY c.city_id,month(tp.month);
+
 -- RATING TARGET--
+
+
 
 -- TOURISM BASEED-- 
 SELECT c.city_name AS City ,MONTHNAME(t.month) AS month,t.avg_passenger_rating,tpr.target_avg_passenger_rating
@@ -203,8 +198,7 @@ GROUP BY 1,2
 INNER JOIN dim_city c ON tpr.city_id=c.city_id
 WHERE c.city_name NOT IN ("Jaipur","Kochi","Mysore","Chandigarh");
 
-/* HIGHEST AND LOWEST REPEAT PASSENGER RATE FOR EACH CITY ACROSS THE SIX MONTH PERIOD.IDENTIFY 
-THE TOP 2 AND BOTTOM 2 CITIES BASED ON  THEIR RPR TO DETERMINE THE LOCATIONS HAVE THE STRONGEST AND WEAKEST RATES.*/
+-- HIGHEST AND LOWEST REPEAT PASSENGER RATE FOR EACH CITY ACROSS THE SIX MONTH PERIOD.IDENTIFY  THE TOP 2 AND BOTTOM 2 CITIES BASED ON  THEIR RPR TO DETERMINE THE LOCATIONS HAVE THE STRONGEST AND WEAKEST RATES.*/
 WITH CTE AS 
 (
 SELECT city_name,SUM(repeat_passengers) AS total_repeat_passengers,SUM(total_passengers) AS total_passengers,
@@ -216,10 +210,8 @@ GROUP BY city_name
 )
 SELECT city_name,repeat_rate FROM CTE
 WHERE drnk <=2 OR drnk>=9;
-/*
-SIMILARLY ANALYSE THE RPR% FOR MONTHS ACROSS ALL CITIES AND IDENTIFY THE MONTHS WITH HIGHEST AND LOWEST RPR ACROSS EACH
-CITY
-*/
+
+-- SIMILARLY ANALYSE THE RPR% FOR MONTHS ACROSS ALL CITIES AND IDENTIFY THE MONTHS WITH HIGHEST AND LOWEST RPR ACROSS EACH CITY
 WITH CTE AS
 (
 SELECT MONTHNAME(month) AS month_name,city_id,total_passengers,repeat_passengers,
